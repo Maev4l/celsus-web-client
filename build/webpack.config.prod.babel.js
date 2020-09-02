f76ac18069a18db9a6f89c3f5d95f38ba25b1c94
Maev4l <maeval.nightingale@gmail.com>
2020-09-02 16:07:11 +0200

    Minify code

--- build/webpack.config.prod.babel.js
+++ build/webpack.config.prod.babel.js
@@ -1,9 +1,16 @@
 import webpack from 'webpack';
 import { merge } from 'webpack-merge';
+import TerserPlugin from 'terser-webpack-plugin';
 
 import baseConfig from './webpack.config.base.babel';
 
 export default merge(baseConfig, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({ parallel: true, sourceMap: true, cache: true, extractComments: false }),
+    ],
+  },
   plugins: [],
 });
