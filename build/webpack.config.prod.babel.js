98c6a776ea01dd2dfe37147132065177895c9755
Maev4l <maeval.nightingale@gmail.com>
2020-09-02 16:37:44 +0200

    Code splitting

--- build/webpack.config.prod.babel.js
+++ build/webpack.config.prod.babel.js
@@ -11,6 +11,24 @@ export default merge(baseConfig, {
     minimizer: [
       new TerserPlugin({ parallel: true, sourceMap: true, cache: true, extractComments: false }),
     ],
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
   },
   plugins: [],
 });
