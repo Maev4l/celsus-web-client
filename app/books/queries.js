5badfce7cb73ef40504beccabd1daa0666640410
Maev4l <maeval.nightingale@gmail.com>
2020-09-14 11:07:57 +0200

    Page size for books list is controlled by the client

--- app/books/queries.js
+++ app/books/queries.js
@@ -1,5 +1,5 @@
-export const FetchLibraryBooks = `query FetchLibraryBooks ($id: ID!, $page:Int!) {
-    library(id:$id, page:$page) {
+export const FetchLibraryBooks = `query FetchLibraryBooks ($id: ID!, $page:Int = 1, $pageSize: Int = 20) {
+    library(id:$id, page:$page, pageSize:$pageSize) {
         content {
             books {
                 id,
