3fac5a49df7bb2dc131b61a64699b048ae8e01f9
Maev4l <maeval.nightingale@gmail.com>
2020-09-15 16:59:02 +0200

    Add ImageUploader component

---
+++ app/shared/ui/ImageUploader.jsx
@@ -0,0 +1,89 @@
+import React, { useRef, useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardMedia, CardActionArea } from '@material-ui/core';
+
+const shapes = {
+  'rectangular-large': {
+    width: 160,
+    height: 240,
+  },
+  'rectangular-medium': {
+    width: 80,
+    height: 120,
+  },
+  'rectangular-small': {
+    width: 40,
+    height: 60,
+  },
+
+  'square-large': {
+    width: 132,
+    height: 132,
+  },
+  'square-medium': {
+    width: 64,
+    height: 64,
+  },
+  'square-small': {
+    width: 32,
+    height: 32,
+  },
+};
+
+const ImageUploader = ({ shape, size, border, onContentChanged }) => {
+  const useStyles = makeStyles({
+    root: { margin: 0, ...shapes[`${shape}-${size}`] },
+    input: {
+      opacity: '0 !important',
+      width: '0 !important',
+      height: '0 !important',
+      padding: '0 !important',
+      margin: '0 !important',
+      border: '0 !important',
+    },
+    img: {
+      ...shapes[`${shape}-${size}`],
+    },
+  });
+  const { root, input, img } = useStyles({ border });
+  const inputRef = useRef(null);
+  const [image, setImage] = useState();
+
+  const handleFileChange = (e) => {
+    const {
+      target: { files },
+    } = e;
+    const [file] = files;
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (evn) => {
+        const encodedImage = Buffer.from(evn.target.result).toString('base64');
+        setImage(`data:image/png;base64,${encodedImage}`);
+
+        if (onContentChanged) {
+          onContentChanged(encodedImage);
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
+
+  const handleImageClick = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <Card className={clsx(root)}>
+      <input className={clsx(input)} ref={inputRef} type="file" onChange={handleFileChange} />
+      {/* eslint-disable */}
+      <CardActionArea onClick={handleImageClick}>
+        <CardMedia className={clsx(img)} image={image} />
+      </CardActionArea>
+      {/* eslint-enable */}
+    </Card>
+  );
+};
+
+export default ImageUploader;
--- app/shared/ui/index.js
+++ app/shared/ui/index.js
@@ -1,2 +1,3 @@
 export { default as SimpleModal } from './SimpleModal';
 export { default as Loading } from './Loading';
+export { default as ImageUploader } from './ImageUploader';
