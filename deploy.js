f7380822c344f87a135656b31a4f539086f7e3e1
Maev4l <maeval.nightingale@gmail.com>
2020-12-13 10:39:43 +0100

    Fix source for S3 deployment
    We cannot use custom source with --react option

--- deploy.js
+++ deploy.js
@@ -6,6 +6,6 @@ const { execSync } = require('child_process');
 const infra = require('./infra.json');
 
 execSync(
-  `deploy-aws-s3-cloudfront --source ./dist --bucket celsus.isnan.eu --distribution ${infra.webClientDistribution} --invalidation-path "/*" --profile default --delete --non-interactive --react`,
+  `deploy-aws-s3-cloudfront --source ./dist --bucket celsus.isnan.eu --distribution ${infra.webClientDistribution} --invalidation-path "/*" --profile default --delete --non-interactive --cache-control index.html:no-cache`,
   { stdio: 'inherit' },
 );
